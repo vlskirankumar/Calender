75343160ea5e1fbf23f133c3effff5f55f69a033
vlskirankumar <44924743+vlskirankumar@users.noreply.github.com>
2024-09-08 21:49:11 +0530

    Pantry API with build bundles

--- src/CalendarComponent.tsx
+++ src/CalendarComponent.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { ClassName, TileArgs } from 'react-calendar/dist/cjs/shared/types';
@@ -11,6 +11,7 @@ interface Event {
 };
 
 const CalendarComponent = () => {
+    const [apiKey, setApiKey] = useState<string | null>(localStorage.getItem("attendancePantryApiKey"));
     const [selectedDate, setSelectedDate] = useState<string>("");
     const [selectedEventType, setSelectedEventType] = useState<EventType>();
     const [showDialog, setShowDialog] = useState<boolean>(false);
@@ -31,13 +32,49 @@ const CalendarComponent = () => {
 
     const lastDate = useMemo(() => {
         const retDate = new Date(today);
-        if (retDate.getDay() !== 6) {
-            retDate.setDate(retDate.getDate() - retDate.getDay());
-        } else {
-            retDate.setDate(retDate.getDate() + 1);
-        }
+        // if (retDate.getDay() !== 6) {
+        //     retDate.setDate(retDate.getDate() - retDate.getDay());
+        // } else {
+        //     retDate.setDate(retDate.getDate() + 1);
+        // }
         return retDate;
     }, [today]);
+
+    useEffect(() => {
+        if (apiKey) {
+            fetch(`https://getpantry.cloud/apiv1/pantry/${apiKey}/basket/attendance`, { method: "GET" }).then((resp) => {
+                resp.json().then(j => {
+                    const eventsData = j.data as Event[];
+                    if (eventsData && eventsData.length > 0) {
+                        setEvents(eventsData.filter(e => {
+                            const dt = new Date(e.date);
+                            return (dt <= lastDate) && (dt >= firstDate)
+                                && (dt.getDay() !== 6) && (dt.getDay() !== 0)
+                        }));
+                    }
+                });
+            });
+        }
+    }, [apiKey, firstDate, lastDate]);
+
+    const saveToPantry = async () => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        const raw = JSON.stringify({ "data": events });
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        } as RequestInit;
+        fetch("https://getpantry.cloud/apiv1/pantry/" + apiKey + "/basket/attendance", requestOptions)
+            .then((response) => response.text())
+            .then((result) => alert(result))
+            .catch((error) => {
+                console.error(error);
+                alert(error);
+            });
+    };
 
     const handleDateClick = (date: Date, event: React.MouseEvent<HTMLButtonElement>) => {
         setShowDialog(true);
@@ -75,28 +112,68 @@ const CalendarComponent = () => {
         setShowDialog(false);
     };
 
-    const handleLoadData = (changeEvent: React.ChangeEvent<HTMLTextAreaElement>) => {
-        try {
-            const data = JSON.parse(changeEvent.target.value);
-            setEvents(data);
-        } catch {
-            alert("Invalid JSON");
-        }
+    const disableWeekends = ({ date }: TileArgs) => {
+        const day = date.getDay();
+        return day === 0 || day === 6; // 0 for Sunday, 6 for Saturday
     };
 
-    return (
-        <div>
+    const officeDays = useMemo(() => {
+        const lastWeekEnd = new Date(today);
+        if(lastWeekEnd.getDay() === 6){
+            lastWeekEnd.setDate(lastWeekEnd.getDate() - lastWeekEnd.getDay());
+        }
+        return events.filter(e => e.type === "office" && new Date(e.date) < lastWeekEnd).length;
+    }, [events, today]);
+
+    const oooDays = useMemo(() => {
+        const lastWeekEnd = new Date(today);
+        if(lastWeekEnd.getDay() === 6){
+            lastWeekEnd.setDate(lastWeekEnd.getDate() - lastWeekEnd.getDay());
+        }
+        return events.filter(e => (e.type === "holiday" || e.type === "ooo") && new Date(e.date) < lastWeekEnd).length;
+    }, [events, today]);
+
+    const wfhDays = useMemo(() => {
+        const lastWeekEnd = new Date(today);
+        if(lastWeekEnd.getDay() === 6){
+            lastWeekEnd.setDate(lastWeekEnd.getDate() - lastWeekEnd.getDay());
+        }
+        return events.filter(e => e.type === "wfh" && new Date(e.date) < lastWeekEnd).length;
+    }, [events, today]);
+
+    const percetage = useMemo(() => ((100 * (officeDays + oooDays))/(60 - wfhDays)).toFixed(2), [officeDays, oooDays, wfhDays]);
+
+    if (apiKey) {
+        return <div>
             <h1>Calendar</h1>
             <div style={{ display: "flex" }}>
                 <div>
-                    <Calendar onClickDay={handleDateClick} maxDate={lastDate} minDate={firstDate} tileClassName={getDateStyle} />
-                    <textarea rows={5} cols={45} value={JSON.stringify(events)} onChange={handleLoadData}></textarea>
+                    <Calendar onClickDay={handleDateClick} maxDate={lastDate} minDate={firstDate}
+                        tileClassName={getDateStyle} tileDisabled={disableWeekends} />
+                    <input type='button' value="Save" onClick={saveToPantry} />
+                    <table border={2}>
+                        <thead>
+                            <tr>
+                                <th>In Office</th>
+                                <th>Leave + Holidays</th>
+                                <th>Approved WFH + Comp Leave</th>
+                                <th>Percentage</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{officeDays}</td>
+                                <td>{oooDays}</td>
+                                <td>{wfhDays}</td>
+                                <td>{percetage + " %"}</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <div>
                     {/* <Calendar onClickDay={handleDateClick} /> */}
                 </div>
             </div>
-            
             {
                 showDialog &&
                 <div style={{ position: "fixed", height: "100%", width: "100%", backgroundColor: "white", top: 0, left: 0 }}>
@@ -115,7 +192,17 @@ const CalendarComponent = () => {
                 </div>
             }
         </div>
-    );
+    } else {
+        return <div style={{ margin: "auto", marginTop: "30vh" }}>
+            <input size={32} onChange={e => {
+                const regex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[4][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+                if (regex.test(e.target.value)) {
+                    setApiKey(e.target.value);
+                    localStorage.setItem("attendancePantryApiKey", e.target.value);
+                }
+            }} placeholder='Please Enter Pantry API Key!' />
+        </div>
+    }
 };
 
 export default CalendarComponent;
